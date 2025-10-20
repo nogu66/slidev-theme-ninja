03e4dd1a4e90bca2f515ad68f4e1460358be29ef
nogu66 <ezochi66@gmail.com>
2025-10-20 17:15:07 +0900

    first commit

---
+++ components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button>
+        <slot />
+    </button>
+</template>
+
+<style scoped>
+button {
+    color: var(--ninja-background);
+    background-color: var(--ninja-foreground);
+    border: 1px solid var(--ninja-foreground);
+    border-radius: var(--ninja-radius);
+    padding: 6px 12px;
+    @apply text-sm;
+}
+button:hover {
+    color: var(--ninja-foreground);
+    background-color: var(--ninja-background);
+}
+</style>
---
+++ docs/built-ins/default-configuration.md
@@ -0,0 +1,9 @@
+# Default Configuration
+
+Slidev Theme Ninja provides some default Slidev configuration values.
+
+## Fonts
+
+| Sans                            | Mono                                            |
+| ------------------------------- | ----------------------------------------------- |
+| [Inter](https://rsms.me/inter/) | [Fira Code](https://github.com/tonsky/FiraCode) |
